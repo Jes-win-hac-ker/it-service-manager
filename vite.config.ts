d4d8cc4a4f3029b30ad09cebd685ad6caafcfab6
Jeswin C  J <100462888+Jes-win-hac-ker@users.noreply.github.com>
2025-08-15 01:01:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'IT Service Manager',
-        short_name: 'ServiceMgr',
-        description: 'An application to manage IT service reports.',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
 })
